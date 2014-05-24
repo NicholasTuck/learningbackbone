16689ba425dec326ecbd6cb3897b798e67066919
Nicholas Tuck <nick@nicholastuck.com>
2014-05-24 12:46:34 -0500

    add text plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 define(function(require){
     "use strict";
 
+    require('plugins');
     var Backbone = require("backbone"),
         _ = require("lodash"),
         $ = require("jquery"),
--- js/requireConfig.js
+++ js/requireConfig.js
@@ -4,7 +4,12 @@ var require = {
         icanhaz: 'vendor/ICanHaz-0.10.2.min',
         lodash: 'vendor/lodash-2.4.1.min',
         jquery: 'vendor/jquery-1.10.2.min',
-        marionette: 'vendor/backbone.marionette-1.8.6.min'
+        marionette: 'vendor/backbone.marionette-1.8.6.min',
+
+        text: 'vendor/text-2.0.12',
+
+        // require plugins
+        plugins: 'plugins'
     },
 
 
