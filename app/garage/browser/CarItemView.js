e91625799c825591c38d205dc74a2b168c7f7a42
Nicholas Tuck <nick@nicholastuck.com>
2014-05-28 22:53:32 -0500

    Keep selected car active after car rename

--- app/garage/browser/CarItemView.js
+++ app/garage/browser/CarItemView.js
@@ -13,7 +13,8 @@ define(function(require) {
     var CarItemView = Marionette.ItemView.extend({
         template: ich.carItem,
         initialize: function() {
-            Backbone.Courier.add( this );
+                Backbone.Courier.add( this );
+            this.listenTo(this.model, 'change', this.onCarChange);
         },
         onRender: function () {
             BackboneUtil.removeWrapperElement(this);
@@ -24,6 +25,11 @@ define(function(require) {
         onClick: function(event) {
             this.spawn("car:selected", {car: this.model});
             this.$el.addClass('active');
+        },
+        onCarChange: function () {
+            var active = this.$el.hasClass('active');
+            this.render();
+            if (active) this.$el.addClass('active');
         }
     });
 
